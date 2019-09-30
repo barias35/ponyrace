62e5337585d215a469eaf03e95ab7cba5c4600bc
Bresly <ing.bresly@hotmail.com>
2019-09-29 22:14:00 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ns-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>Greetings!</h1>
+  <p>{{myFirstGreet}}</p>
+  <ns-races></ns-races>
+  `
 })
 export class AppComponent {
   title = 'ponyracer';
+  numberOfUsers = 146;
+  myFirstGreet : string = "Hello World";
 }
